b1311a37b444d97599300a4b9f1638ae25451e1e
SaaraKhalid <khalidsaara6@gmail.com>
2024-04-07 15:28:56 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -1,19 +1,61 @@
+# server/models.py
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData()
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
 
 db = SQLAlchemy(metadata=metadata)
 
 
-class Pet(db.Model, SerializerMixin):
+class Zookeeper(db.Model, SerializerMixin):
+    __tablename__ = 'zookeepers'
 
-    __tablename__ = 'pets'
+    # don't forget that every tuple needs at least one comma!
+    serialize_rules = ('-animals.zookeeper',)
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, unique=True)
+    birthday = db.Column(db.Date)
+
+    animals = db.relationship('Animal', back_populates='zookeeper')
+
+
+class Enclosure(db.Model, SerializerMixin):
+    __tablename__ = 'enclosures'
+
+    serialize_rules = ('-animals.enclosure',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship('Animal', back_populates='enclosure')
+
+
+class Animal(db.Model, SerializerMixin):
+    __tablename__ = 'animals'
+
+    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
     species = db.Column(db.String)
 
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    enclosure = db.relationship('Enclosure', back_populates='animals')
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+
     def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+        return f'<Animal {self.name}, a {self.species}>'
